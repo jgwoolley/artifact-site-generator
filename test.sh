6a46f96158c2bf6bcd2716bbf9e8199e865bb2dd
James W <26191568+jgwoolley@users.noreply.github.com>
2026-08-16 23:01:36 -0400

    Messed with bash scripts

--- test.sh
+++ test.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
 
-mvn package
+# mvn package
 
 java -jar ./artifact-site-cli/target/artifact-site-cli-*.jar clear-plugins
 
+java -jar ./artifact-site-cli/target/artifact-site-cli-*.jar add-plugin ./artifact-site-plugin-maven/target/artifact-site-plugin-maven-*.jar
+
 java -jar ./artifact-site-cli/target/artifact-site-cli-*.jar add-plugin ./artifact-site-plugin-vsix/target/artifact-site-plugin-vsix-*.jar
 
-if [[ ! -f nf3t.nifi-flowfile-extension-0.0.5.vsix ]];then
-    wget https://open-vsx.org/api/nf3t/nifi-flowfile-extension/0.0.5/file/nf3t.nifi-flowfile-extension-0.0.5.vsix -O nf3t.nifi-flowfile-extension-0.0.5.vsix
-fi
+java -jar ./artifact-site-cli/target/artifact-site-cli-*.jar parse ./**/target/artifact-site-plugin-*.jar
 
 java -jar ./artifact-site-cli/target/artifact-site-cli-*.jar parse https://open-vsx.org/api/nf3t/nifi-flowfile-extension/0.0.5/file/nf3t.nifi-flowfile-extension-0.0.5.vsix
 
 java -jar ./artifact-site-cli/target/artifact-site-cli-*.jar generate
-
-python3 -m http.server -d ./public/
